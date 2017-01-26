e66501c0b5ae554bccef7e0507b506ab233a9b8c
mrtomsfly <tomas_steenbuch@hotmail.com>
2017-01-27 00:43:04 +0100

    bad om ursäkt till joe i en kommentar i filen

--- Project4/Project4/main.cpp
+++ Project4/Project4/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	cout << "Hello World!" << endl;
 	cout << "Sarah har varit inne och pillat på koden" << endl;
+	//ledsen joe, tror jag ändrade för mycket
 
 	system("pause");
 	return 0;
