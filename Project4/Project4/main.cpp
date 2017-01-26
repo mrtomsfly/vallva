bd2415326d7ad2f6585de19c2629a18aa3ff8c3e
mrtomsfly <tomas_steenbuch@hotmail.com>
2017-01-27 00:41:54 +0100

    Skrev att sarah ska sluta ändra kod

--- Project4/Project4/main.cpp
+++ Project4/Project4/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 	cout << "Hello World!" << endl;
-	cout << "Sarah har varit inne och pillat på koden" << endl;
+	//"Sarah har varit inne och pillat på koden"
+	cout << "Detta är joe, sluta använd min kod" << endl;
 
 	system("pause");
 	return 0;
